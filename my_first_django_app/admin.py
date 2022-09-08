26de38fd136b8fb3f827cda1c70aaf68de3b999b
pratheeth <pratheeths@gmail.com>
2022-09-08 22:47:14 +0530

    Registered Models and did admin configurations.

--- my_first_django_app/admin.py
+++ my_first_django_app/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.utils import timezone
+
+from . import models
+
+
+def mark_complete(model_admin, request, queryset):
+    # Used to customize the dropdown in TABLE page
+    queryset.update(status=models.Task.TaskStatus.COMPLETED,
+                    completed_at=timezone.now())
+
+
+def mark_pending(model_admin, request, queryset):
+    # Used to customize the dropdown in TABLE page
+    queryset.update(status=models.Task.TaskStatus.PENDING,
+                    completed_at=None)
+
+
+# Adding a short description to the dropdown in a respective Table page
+mark_pending.short_description = "Mark these task as PENDING"
+
+
+class TaskAdmin(admin.ModelAdmin):
+    # We can manipulate the fields to be shown when we are creating a record
+    fields = ['content', 'deadline', 'tags']
+
+    # Add attributes on the main page of the table
+    # We can add attributes and properties
+    list_display = ['content', 'status', 'created_at', 'foo']
+
+    # We can make the attributes / properties in the list_display editable
+    # The attributes / properties mentioned here has to be mandatory present in list_display
+    list_editable = ['status']
+
+    actions = [mark_complete, mark_pending]
+
+    # Create a filter to filters the documents to be displayed
+    # We can have more than 1 filter
+    list_filter = ['status', 'tags']
+
+    # Create a search field
+    search_fields = ['content', 'tags__name']
+
+    # Display the records in a certain order
+    # Approach - 1
+    # ordering = ['deadline']
+
+    # Approach -2
+    def get_ordering(self, request):
+        if request.user.is_superuser:
+            return ['status']
+        else:
+            return ['deadline']
+
+
+admin.site.register(models.Tag)
+admin.site.register(models.Task, TaskAdmin)
--- my_first_django_app/models.py
+++ my_first_django_app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 # Create your models here.
 
 # ORM --> Object Relational Mapping
@@ -8,16 +7,24 @@ from django.db import models
 
 # Model for To-Do app
 # Inherit from Model
+from django.utils import timezone
+
+
 class Tag(models.Model):
     name = models.CharField(max_length=250)
+
+    def __str__(self):
+        # This is equivalent to object.toString() in Java
+        # Gives a human-readable representation of the object
+        return self.name
 
 
 class Task(models.Model):
     content = models.TextField()
-    deadline = models.DateTimeField()
-    created_at = models.DateTimeField()
-    completed_at = models.DateTimeField()
-    tags = models.ManyToManyField(Tag)
+    deadline = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    completed_at = models.DateTimeField(null=True, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
 
     class TaskStatus(models.TextChoices):  # This is enumeration
         COMPLETED = "CO", "COMPLETED"
@@ -29,6 +36,16 @@ class Task(models.Model):
         default=TaskStatus.PENDING,
         max_length=2,
     )  # The choices must be restricted
+
+    @property
+    def foo(self):
+        return "Hello"
+
+    def __str__(self):
+        return f'{self.content} - {self.get_status_display()}'
+
+    def get_status_display(self):
+        pass
 
 # Relationship between Task and Tag
 # 1 task can have 1/more tags and 1 tag can have 1/more tasks
